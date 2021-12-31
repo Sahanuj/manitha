37368953f3f62414ca6f72ee6b1878b5444f111e
Sahanuj <82720442+Sahanuj@users.noreply.github.com>
2021-12-31 16:13:10 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -28,7 +28,7 @@ from requests.exceptions import ConnectionError
 from mechanize import Browser
 from importlib import reload
 reload(sys)
-sys.setdefaultencoding('utf8')
+sys.getdefaultencoding()
 br = mechanize.Browser()
 br.set_handle_robots(False)
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
